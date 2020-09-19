1e5cf63202bb1674c888c131ebd61c3cca6f8578
Safuan Yaylyaev <yaylyaevsafuan@gmail.com>
2020-09-19 09:26:55 +0300

    - method __construct

--- App/Controllers/Error.php
+++ App/Controllers/Error.php
@@ -4,16 +4,9 @@ namespace App\Controllers;
 
 class Error extends BaseController
 {
-    public function __construct(\Exception $error)
-    {
-        parent::__construct();
-        $this->view->error = $error;
-    }
-
     protected function action()
     {
         $this->view->twigDisplay('/App/templates/error.html', [
-            'error' => $this->view->error,
             'user' => $this->view->user,
         ]);
     }
