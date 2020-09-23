1e7f848c4a653398b06640e0f7b421fdee62fdf9
Safuan Yaylyaev <yaylyaevsafuan@gmail.com>
2020-09-23 20:44:18 +0300

    +show comments for image

--- App/Modules/Image/Controllers/Index/One.php
+++ App/Modules/Image/Controllers/Index/One.php
@@ -4,6 +4,7 @@ namespace App\Modules\Image\Controllers\Index;
 
 use App\Controllers\BaseController;
 use App\Exceptions\Error404;
+use App\Models\Comment;
 use App\Models\Image;
 
 class One extends BaseController
@@ -11,19 +12,23 @@ class One extends BaseController
 
     protected function action()
     {
-        $id = $_GET['id'];
-        if (empty($id) || !is_numeric($id)) {
+        $recordId = $_GET['id'];
+        if (empty($recordId) || !is_numeric($recordId)) {
             throw new Error404('Некорректно введены данные, страница не найдена!', 404);
         }
 
-        $image = Image::findById($id);
+        $image = Image::findById($recordId);
 
         if (false === $image) {
             throw new Error404('Страница не найдена!', 404);
         }
 
+        $moduleName = Image::TABLE;
+        $comments = Comment::findByRecord($moduleName, $recordId);
+
         $this->view->twigDisplay('/App/Modules/Image/templates/index/image.html', [
             'image' => $image,
+            'comments' => $comments,
             'user' => $this->view->user,
         ]);
     }
